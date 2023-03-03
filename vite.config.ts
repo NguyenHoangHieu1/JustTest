fb76dbbaef84f39669756ee4d221ab2472d28a45
hieu <tramyfro@gmail.com>
2023-03-03 12:30:34 +0700

    final change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,5 @@ import svgrPlugin from "vite-plugin-svgr"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: "/JustTest/",
 })
